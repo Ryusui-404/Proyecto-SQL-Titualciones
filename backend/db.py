1d5bbec4dc1e014918396358fe1c85dce992fdfd
Ryusui-404 <bsanchez26@ucol.mx>
2025-09-08 20:35:43 -0600

    Update db.py
    Removed server address from connection string.

--- backend/db.py
+++ backend/db.py
@@ -3,10 +3,11 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=DESKTOP-HI6U3GK\SQLEXPRESS;"
+        "SERVER=;"
         "DATABASE=TitulacionesBDNR;"
         "UID=sa;"
         "PWD="
     )
     return conn
+
 
